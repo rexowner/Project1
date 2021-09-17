fd09a976191815c4ff90cfebf8ba60feb537415f
Les Poltrack <lapwack@gmail.com>
2021-09-17 13:09:59 -0700

    Initial Commit

---
+++ Shared/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Shared
+//
+//  Created by Les Poltrack on 9/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
